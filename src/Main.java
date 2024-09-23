eef2a687049bc5c79df26330a33c78258bdd649d
Ramplastinskij <roman.zhilkin.21@yandex.ru>
2024-09-24 00:20:36 +0500

    Basis for the Project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {}
+    private final static Random RANDOM = new Random();
+    private final static String[] NAMES = {"Сергей", "Антон", "Роман", "Александр", "Михаил", "Ярослав"};
+    private final static String[] SURNAMES = {"Ульянов", "Багромян", "Рокоссовский", "Жуков", "Панов", "Тихомиров"};
+    private final static String[] PATRONYMICS_NAMES = {"Федорович", "Геннадьевич", "Алексеевич", "Георгиевич", "Владимирович", "Вячеславович"};
+
+    private final static Employee[] EMPLOYEES = new Employee[10];
+
+    public static void initEmployees() {
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            String fullName = SURNAMES[RANDOM.nextInt(0, SURNAMES.length)] + " " +
+                    NAMES[RANDOM.nextInt(0, NAMES.length)] + " " +
+                    PATRONYMICS_NAMES[RANDOM.nextInt(0, PATRONYMICS_NAMES.length)];
+            EMPLOYEES[i] = new Employee(fullName, RANDOM.nextInt(1, 6), RANDOM.nextInt(50_000, 250_000));
+        }
+    }
+
+    public static void main(String[] args) {
+        initEmployees();
+        print();
+    }
+
+    public static void print() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee);
+        }
+    }
 }
